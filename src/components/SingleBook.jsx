8bdc6dad739b5d5faed2e0d3f959a98dd08335d5
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-02-05 16:49:50 +0000

    punto 5

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,18 +1,32 @@
+import { Component } from "react";
 import { Button, Card } from "react-bootstrap";
 
-const SingleBook = (props) => {
-  return (
-    <Card>
-      <Card.Img variant="top" src={props.img} style={{ maxWidth: "200px", marginInline: "auto" }} className="p-2" />
-      <Card.Body>
-        <Card.Title className="d-flex align-items-center" style={{ height: "150px" }}>
-          {props.title}
-        </Card.Title>
-        <Card.Text>Prezzo: {props.price} €</Card.Text>
-        <Button variant="warning">Acquista</Button>
-      </Card.Body>
-    </Card>
-  );
-};
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+  render() {
+    return (
+      <Card style={this.state.selected ? { border: "2px solid red" } : {}}>
+        <Card.Img
+          onClick={() => {
+            this.setState({ selected: true });
+          }}
+          variant="top"
+          src={this.props.img}
+          style={{ maxWidth: "200px", marginInline: "auto" }}
+          className="p-2"
+        />
+        <Card.Body>
+          <Card.Title className="d-flex align-items-center" style={{ height: "150px" }}>
+            {this.props.title}
+          </Card.Title>
+          <Card.Text>Prezzo: {this.props.price} €</Card.Text>
+          <Button variant="warning">Acquista</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default SingleBook;
